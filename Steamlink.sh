ca549af1bf082e7d670247fdee44f4f098df9f8a
Jeod <47716344+JeodC@users.noreply.github.com>
2025-01-28 12:49:34 -0500

    Initial commit

---
+++ Steamlink.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
+
+# Variables
+GAMEDIR="/$directory/ports/steamlink"
+
+# CD and set permissions
+cd $GAMEDIR
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+$ESUDO chmod +x -R $GAMEDIR/*
+
+# Exports
+export LD_LIBRARY_PATH="$GAMEDIR/lib:$GAMEDIR/Qt-5.14.1/lib:$LD_LIBRARY_PATH"
+export GROUP="plugdev"
+export MODE="0660"
+
+# Assign gptokeyb and load the game
+pm_platform_helper "$GAMEDIR/bin/shell" >/dev/null
+./bin/shell
+
+# Cleanup
+pm_finish
